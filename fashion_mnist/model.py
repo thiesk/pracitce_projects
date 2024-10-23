955deccdb3005bd91be0d30555c73afabd0e1db5
thipeker <thies.kersten@gmail.com>
2024-10-23 21:54:59 +0200

    first simplistic conv_net model

--- fashion_mnist/model.py
+++ fashion_mnist/model.py
@@ -1,12 +1,36 @@
-import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class convolution_net(nn.Module):
+class Convolution_net(nn.Module):
+    '''
+    Stupid simple convolutional network.
+    Some convolution layers and fc classification head.
+    '''
     def __init__(self):
-        self.resolution = 28 * 28
-        self.cl1 = nn.Conv2d(1, 21, 3)
-        self.cl2 = nn.Conv2d(32, 16, 3)
-        self.cl3 = nn.Conv2d(16, 8, 3)
+        super(Convolution_net, self).__init__()
 
-        self.fc1 = nn.Linear(8 * self.resolution, 100)
-        self.fc2 = torch.Linear(100, 10)
+        self.cl1 = nn.Conv2d(1, 32, 3)
+        self.cl2 = nn.Conv2d(32, 16, 3)
+
+        self.fc1 = nn.Linear(400, 100)
+        self.fc2 = nn.Linear(100, 10)
+
+
+
+    def forward(self, x):
+        # first conv block
+        x = self.cl1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2, 2)
+        # second conv block
+        x = self.cl2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2, 2)
+
+        # fc layer for classification
+        x = x.flatten(1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+
+        return x
