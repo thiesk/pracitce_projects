b4ed0fdf78990b26e2e5defb32e773fdf4d535e4
thipeker <thies.kersten@gmail.com>
2024-10-23 21:53:48 +0200

    add tensor 'nothing transform'

--- fashion_mnist/dataset.py
+++ fashion_mnist/dataset.py
@@ -1,5 +1,5 @@
 import torchvision
-import seaborn as isns
+from torchvision import transforms
 from torch.utils.data import Dataset
 
 def get_transform(split='train'):
@@ -8,15 +8,19 @@ def get_transform(split='train'):
     :param split: split of what split the transforms are for
     :return:
     '''
-    return None
+    aug = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    return aug
 
 
 class fashionMnist(Dataset):
-    def __init__(self, ):
+    def __init__(self, train=True):
         self.transform = get_transform()
-        self.data = torchvision.datasets.FashionMNIST(root="/data", download=True, transform=get_transform())
+        self.data = torchvision.datasets.FashionMNIST(root="/data", train=train, download=True, transform=self.transform)
 
     def __len__(self):
         return len(self.data)
+
     def __getitem__(self, idx):
         return self.data[idx]
